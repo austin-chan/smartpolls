8bfd52c3defdab397cdb9613936198b70def5c84
Austin Chan <auscwork@gmail.com>
2016-02-20 16:32:30 -0500

    firebase structure

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require("path");
+
+module.exports = {
+    entry: './entry.js',
+    output: {
+        filename: 'bundle.js', //this is the default name, so you can skip it
+        //at this directory our bundle file will be available
+        //make sure port 8090 is used when launching webpack-dev-server
+        publicPath: 'http://localhost:8090/assets'
+    },
+    module: {
+        loaders: [
+            // {
+            //     //tell webpack to use jsx-loader for all *.jsx files
+            //     test: /\.jsx$/,
+            //     loader: 'jsx-loader?insertPragma=React.DOM&harmony'
+            // },
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
+            },
+            {
+                loader: "babel-loader",
+                exclude: [
+                    path.resolve(__dirname, "node_modules"),
+                ],
+                test: /\.jsx?$/,
+                query: {
+                    plugins: ['transform-runtime'],
+                    presets: ['es2015', 'stage-0', 'react'],
+                }
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+}
