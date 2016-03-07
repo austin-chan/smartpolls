0226e6ce56a84fbb84a30225304028ce4f109307
Austin Chan <auscwork@gmail.com>
2016-03-07 17:13:16 -0500

    adding to the poll session page

---
+++ components/Poll.js
@@ -0,0 +1,62 @@
+import React, { Component, PropTypes } from 'react';
+import '../styles/_Poll.scss';
+
+export default class SessionPage extends Component {
+  componentWillMount() {
+  }
+
+  render() {
+    return (
+      <div className="Poll clearfix">
+        <div className="choice-labels">
+          <div className="choice-label">
+            <span className="vertical-aligner"></span>
+            <span className="choice-label-text">A</span>
+          </div>
+          <div className="choice-label">
+            <span className="vertical-aligner"></span>
+            <span className="choice-label-text">B</span>
+          </div>
+          <div className="choice-label">
+            <span className="vertical-aligner"></span>
+            <span className="choice-label-text">C</span>
+          </div>
+          <div className="choice-label">
+            <span className="vertical-aligner"></span>
+            <span className="choice-label-text">D</span>
+          </div>
+          <div className="choice-label">
+            <span className="vertical-aligner"></span>
+            <span className="choice-label-text">E</span>
+          </div>
+        </div>
+        <div className="baseline"></div>
+        <div className="graph">
+          <div className="graph-bars">
+            <div className="graph-bar-wrap">
+              <span className="vertical-aligner"></span>
+              <div className="graph-bar">
+                <span className="percentage">67%</span>
+                <span className="count">2 votes</span>
+              </div>
+            </div>
+            <div className="graph-bar-wrap">
+              <span className="vertical-aligner"></span>
+              <div className="graph-bar">
+                <span className="percentage">67%</span>
+                <span className="count">2 votes</span>
+              </div>
+            </div>
+            <div className="graph-bar-wrap">
+              <div className="graph-bar">
+                <span className="percentage">67%</span>
+                <span className="count">2 votes</span>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- components/SessionPage.js
+++ components/SessionPage.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import Poll from './Poll';
 import '../styles/_SessionPage.scss';
 
 export default class SessionPage extends Component {
@@ -9,18 +10,48 @@ export default class SessionPage extends Component {
   render() {
     return (
       <div id="SessionPage">
-        <div className="session-info card container">
-            <div className="left-side">
-                <h5>Poll Session Code</h5>
-                <p className="session-code">exotic-moon</p>
+        <div className="session-info-card card container">
+          <div className="left-side side">
+            <span className="vertical-aligner"></span>
+            <div className="wrap">
+              <h5>Poll Session Code</h5>
+              <p className="session-code">exotic-moon</p>
+              <div className="button standard-button">Change Session Code</div>
             </div>
-            <div className="middle-side">
-
+          </div>
+          <div className="middle-side side">
+            <span className="vertical-aligner"></span>
+            <div className="wrap">
+              <h5>To join the polling session:</h5>
+              <div className="steps">
+                <p className="step">1. Navigate to smartpolls.co</p>
+                <p className="step">2. Enter code exotic-moon</p>
+              </div>
             </div>
-            <div className="right-side">
-
+          </div>
+          <div className="right-side side">
+            <div className="wrap">
+              <span className="vertical-aligner"></span>
+              <div className="counters">
+                <div className="votes">
+                  <span className="count">8</span>
+                  <span className="label">votes</span>
+                </div>
+                <div className="voters">
+                  <span className="count">5</span>
+                  <span className="label">voters</span>
+                </div>
+              </div>
             </div>
-
+          </div>
+        </div>
+        <div className="poll-card card container">
+          <div className="top-bar">
+            <h2 className="title">Poll #1</h2>
+            <span className="status">Active</span>
+            <div className="standard-button button lock-button">Lock & Show Results</div>
+          </div>
+          <Poll />
         </div>
       </div>
     );
